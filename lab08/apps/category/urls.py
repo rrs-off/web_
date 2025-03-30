7e25fc7160a405d37ec0eb7819c8ccea71a233b0
rrs-off <157564773+rrs-off@users.noreply.github.com>
2025-03-30 19:10:11 +0500

    Delete lab08 directory

--- lab08/apps/category/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, re_path
-
-from category.views import get_categories, get_category
-
-urlpatterns = [
-
-    path('', get_categories),
-    path('<int:pk>/', get_category),
-]
